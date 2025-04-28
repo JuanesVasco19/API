9b6976cdd1bb8462c0f4e36f9b5b9fc811f3b317
JuanesVasco19 <juan.vascol@upb.edu.co>
2025-04-28 17:51:42 -0500

    se crea el primer post

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
   res.send('Adios Mundo Cruel!');
 });
+
  app.get('/api', (req, res) => {
-  res.json({ message: 'Entraste a la Ruta API ... ' });
-    res.json({ message: 'Bienvenido!' });
+  res.send({ message: 'Entraste a la Ruta API ... ' });
   });
 
+
+  
+// un post es para cambiar o agregar datos en el servidor, por ejemplo crear un usuario
+app.post('/api', (req, res) => {
+  //Todo lo que tengo que hacer para crear el usuario
+  res.send('Usuario Creado!');});
