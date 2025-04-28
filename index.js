96daef4e5d75a811a136a858ee03164cb08e60b6
JuanesVasco19 <juan.vascol@upb.edu.co>
2025-04-28 17:17:53 -0500

    se hizo otra ruta al servidor con su respectivo mensaje "Entraste a la ruta API ..."

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 app.get('/', (req, res) => {
   res.send('Adios Mundo Cruel!');
 });
+ app.get('/api', (req, res) => {
+  res.json({ message: 'Entraste a la Ruta API ... ' });
+    res.json({ message: 'Bienvenido!' });
+  });
+
